e1e4f030d2e7b57da91cdc718b097bc31ade4565
yilakayele <nathanj2ee@gmail.com>
2019-02-14 23:32:13 -0500

    first eureka integration chnage

---
+++ src/main/java/com/techprimers/db/dbservices/DbServicesApplication.java
@@ -0,0 +1,14 @@
+package com.techprimers.db.dbservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServicesApplication.class, args);
+	}
+
+}
+
